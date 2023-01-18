d43155a62ca3078c4f47549f4291f7a24cbf346e
Denis <hallo-boy@mail.ru>
2023-01-18 12:47:33 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div className="">
+        <a href="#g">Profile</a>
+      </div>
+      <div className="">
+        <a href="#g">Messages</a>
+      </div>
+      <div className="">
+        <a href="#g">News</a>
+      </div>
+      <div className="">
+        <a href="g#">Music</a>
+      </div>
+      <div className="">
+        <a href="#g">Settings</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-fff2lftqIE077pFAKU1Mhbcj8YFvBbMvpA&usqp=CAU"
+          alt=""
+        />
+      </div>
+      <div className="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Circle-icons-image.svg/800px-Circle-icons-image.svg.png"
+          alt="ava"
+        />{" "}
+        description
+      </div>
+      <div className="">
+        my posts
+        <div className="">new post</div>
+      </div>
+      <div className="">
+        <div className="">post 1</div>
+        <div className="">post 2</div>
+        <div className="">post 3</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
